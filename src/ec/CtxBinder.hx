19cef678d7b607120b33ed06bc8a9078b071e4fa
artem pecheny <pecheny@gmail.com>
2020-10-12 19:05:46 +0300

    Keep commented BinderCollection for possible future use.

--- src/ec/CtxBinder.hx
+++ src/ec/CtxBinder.hx
@@ -35,7 +35,57 @@ class CtxBinder<T:CtxBindable> {
         ctx.bind(entity);
     }
 
+    public function unbind() {
+        if (ctx != null)
+            ctx.unbind(entity);
+    }
+
+    public function rebind() {
+        if (ctx != null)
+            ctx.bind(entity);
+    }
+
 }
+
+//@:access(ec.CtxBinder)
+//@:access(ec.Binders)
+//class Binders {
+//    var classMap:ObjectMap<Dynamic, CtxBinder<Dynamic>> = new ObjectMap();
+//    function new () {}
+//    public static function getBinding<T:CtxBindable>(e:Entity, cl:Class<T>, upwardOnly = false):CtxBinder<T> {
+//        var binders:Binders;
+//        if (e.hasComponent(Binders)) {
+//            binders = e.getComponent(Binders);
+//        } else {
+//            binders = new Binders();
+//            e.addComponent(binders);
+//        }
+//        if (binders.classMap.exists(cl)){
+//            var binder:CtxBinder<T> = cast binders.classMap.get(cl);
+//            if (binder.upwardOnly != upwardOnly)
+//                throw "Already has binder with different 'upwardOnly' value";
+//            return binder;
+//        }
+//        var binder = new CtxBinder<T>(cl, e, upwardOnly);
+//        binders.classMap.set(cl, binder);
+//        return binder;
+//    }
+//
+//    public function unbindAll() {
+//        trace("unbind all");
+//        for (k in classMap.keys()) {
+//            var bdr = classMap.get(k);
+//            bdr.unbind();
+//        }
+//    }
+//
+//    public function rebindAll() {
+//        for (k in classMap.keys()) {
+//            var bdr = classMap.get(k);
+//            bdr.rebind();
+//        }
+//    }
+//}
 
 interface CtxBindable {
     function bind(e:Entity):Void;
