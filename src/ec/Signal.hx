c2cf85a7334c22f8b34969fdae9f3aeebd773a1a
artem pecheny <pecheny@gmail.com>
2021-05-26 22:53:53 +0300

    Fix a bug with unsubscribing during dispatch.

--- src/ec/Signal.hx
+++ src/ec/Signal.hx
@@ -1,11 +1,31 @@
 package ec;
-@:forward(remove)
-abstract Signal<T:haxe.Constraints.Function>(Array<T>) {
-    public inline function new() this = [];
+class Signal<T:haxe.Constraints.Function> {
+    var listeners:Array<T> = [];
+    var isDispatching = false;
+    var toRemove:Array<T> = [];
 
-    public inline function listen(listener:T) this.push(listener);
-    public macro function dispatch(signal, args:Array<haxe.macro.Expr>) {
-        return macro for (listener in $signal.asArray()) listener($a{args});
+    public function new() {
     }
-    public inline function asArray() return this;
+
+    public inline function listen(listener:T) {
+        listeners.push(listener);
+    }
+
+    public macro function dispatch(signal, args:Array<haxe.macro.Expr>) {
+        return macro {
+            @:privateAccess $signal.isDispatching = true;
+            for (listener in $signal.asArray()) listener($a{args});
+            @:privateAccess $signal.isDispatching = false;
+            for (listener in @:privateAccess $signal.toRemove) @:privateAccess $signal.listeners.remove(@:privateAccess $signal.toRemove.shift());
+        }
+    }
+
+    public inline function asArray() return listeners;
+
+    public inline function remove(l:T) {
+        if (isDispatching)
+            toRemove.push(l);
+        else
+            listeners.remove(l);
+    }
 }
