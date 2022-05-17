64facf25bb426c442378cc9be0618b9da84c3b74
artem pecheny <pecheny@gmail.com>
2022-05-17 17:20:20 +0300

    Adopt old tests.

--- test/ec/EntityTest.hx
+++ test/ec/EntityTest.hx
@@ -1,12 +1,56 @@
 package ec;
 import utest.Assert;
+import utest.Assert.isTrue as assert;
 import utest.Test;
 import ec.Entity;
 class EntityTest extends Test {
 
-    // boring
-    // add / get / has by name/type/...
-    // onCtx
+    function test_add_remove_arbitrary_instance() {
+        var c = new Cmp();
+        var e = new Entity();
+        e.addComponent(c);
+        var c2 = e.getComponent(Cmp);
+        assert(e.hasComponent(Cmp));
+        Assert.equals(c, c2);
+        e.removeComponent(Cmp);
+        assert((!e.hasComponent(Cmp)));
+    }
+
+    function test_add_remove_typedef() {
+        var c = new CmpDef();
+        var e = new Entity();
+        e.addComponent(c);
+        var c2 = e.getComponent(Cmp);
+        assert(e.hasComponent(Cmp));
+        Assert.equals(c, c2);
+        e.removeComponent(Cmp);
+        assert((!e.hasComponent(Cmp)));
+    }
+
+    function test_IComponent() {
+        var c = new CmpWEnt();
+        var e = new Entity();
+        e.addComponent(c);
+        var c2:CmpWEnt = e.getComponent(CmpWEnt);
+        assert(e.hasComponent(CmpWEnt));
+        Assert.equals(c, c2);
+        Assert.equals(c2.entity, e);
+        e.removeComponent(CmpWEnt);
+        assert((c2.entity == null));
+        assert(!e.hasComponent(CmpWEnt));
+    }
+
+    function test_custom_component_id() {
+        var name = "name";
+        var c = new NamedCmp(name);
+        var e = new Entity();
+        e.addComponent(c);
+        var c2:NamedCmp = e.getComponentByName(name);
+        assert(!e.hasComponent(NamedCmp));
+        Assert.equals(c, c2);
+        e.removeComponentWithName(name);
+        assert(!e.hasComponentWithName(name));
+    }
 
     function test_getUpward() {
         var root = new Entity();
@@ -51,4 +95,36 @@ class EntityTest extends Test {
 }
 class TestComponent {
     public function new() {}
+}
+
+typedef CmpDef = Cmp;
+class Cmp {
+    public function new() {}
+}
+
+class CmpWEnt implements IComponent {
+    @:isVar public var entity(get, set):Entity;
+
+    public function get_entity():Entity {
+        return entity;
+    }
+
+    public function set_entity(value:Entity):Entity {
+        return this.entity = value;
+    }
+
+    public function new() {}
+}
+
+class NamedCmp implements ICustomComponentId {
+    var name:String;
+
+    public function new(name:String) {
+        this.name = name;
+    }
+
+    public function getId():String {
+        return name;
+    }
+
 }
