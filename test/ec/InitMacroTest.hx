78cfb4c7f331a87a0b8257860e3ee9b12228c34a
artem pecheny <pecheny@gmail.com>
2022-11-24 14:58:22 +0300

    Inheritance support test.

--- test/ec/InitMacroTest.hx
+++ test/ec/InitMacroTest.hx
@@ -1,8 +1,9 @@
 package ec;
+
 import utest.Assert;
 import ec.Entity;
-class InitMacroTest extends utest.Test {
 
+class InitMacroTest extends utest.Test {
     public function test_init_once() {
         var root = new Entity();
         var c1 = new Obj();
@@ -16,30 +17,73 @@ class InitMacroTest extends utest.Test {
         Assert.equals(c1, tester.obj, "Test @:once injection");
         Assert.equals(c2, tester.named, "Test named injection");
     }
+    public function test_all_deps_with_inheritance() {
+        var root = new Entity();
+        var c1 = new Obj();
+        var c2 = new Obj("named");
+        root.addComponent(c1);
+        root.addComponentByName("Obj_named", c2);
+
+        var tester = new ExtendedINjectionTester();
+
+        root.addChild(tester.entity);
+        Assert.equals(0, tester.initCalls);
+        Assert.equals(null, tester.other, "Test @:once injection");
+        Assert.equals(c2, tester.named, "Test named injection");
+
+        root.addComponentByName("Obj_otherNamed", c2);
+
+        Assert.equals(1, tester.initCalls);
+        Assert.equals(c2, tester.other, "Test @:once injection");
+
+    }
+    public function test_all_deps_with_inheritance_reverse_init_order() {
+        var root = new Entity();
+        var c1 = new Obj();
+        var c2 = new Obj("named");
+        root.addComponent(c1);
+        root.addComponentByName("Obj_otherNamed", c2);
+        var tester = new ExtendedINjectionTester();
+
+        root.addChild(tester.entity);
+        Assert.equals(0, tester.initCalls);
+        Assert.equals(c2, tester.other, "Test @:once injection");
+        root.addComponentByName("Obj_named", c2);
+
+        Assert.equals(1, tester.initCalls);
+        Assert.equals(c2, tester.named, "Test named injection");
+    }
+}
+class ExtendedINjectionTester extends InjectionTester {
+    @:once("otherNamed")public var other:Obj;
+    public var initCalls = 0;
+    override public function init() {
+        initCalls++;
+    }
 }
 
-@:build(ec.macros.InitMacro.build())
-class InjectionTester {
+@:autoBuild(ec.macros.InitMacro.build()) class MacroBaseClass {}
+
+class InjectionTester extends MacroBaseClass {
     public var entity(default, null):Entity = new Entity();
     @:once public var obj:Obj;
     @:once("named") public var named:Obj;
-
 
     public function new() {
         entity.onContext.listen(_init);
         _init(entity.parent);
     }
 
-    function _init(e){
-    }
+    function _init(e) {}
 
-    public function init() {
-    }
+    public function init() {}
 }
 
 class Obj {
     static var count = 0;
+
     public var id(default, null):Int;
+
     var name = "noname";
 
     public function new(n = null) {
@@ -48,7 +92,7 @@ class Obj {
         id = count++;
     }
 
-    function toString(){
-        return name  + "_" + id;
+    function toString() {
+        return name + "_" + id;
     }
 }
