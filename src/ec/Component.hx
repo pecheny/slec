12da5a2549aea6ba7533c43e258b146c72a97c05
artem pecheny <pecheny@gmail.com>
2024-02-16 08:39:37 +0300

    Usable Component class.

--- src/ec/Component.hx
+++ src/ec/Component.hx
@@ -1,13 +1,31 @@
 package ec;
-class Component1 implements IComponent {
+import ec.IComponent;
+import ec.Entity;
 
+@:autoBuild(ec.macros.InitMacro.build())
+class Component implements IComponent {
     @:isVar public var entity(get, set):Entity;
+
+    public function new(e:Entity = null) {
+        set_entity(e);
+    }
+
+    function _init(e:Entity) {}
+
+    public function init() {}
 
     public function get_entity():Entity {
         return entity;
     }
 
-    public function set_entity(value:Entity):Entity {
-        return this.entity = value;
+    public function set_entity(entity:Entity):Entity {
+        if (this.entity != null)
+            this.entity.onContext.remove(_init);
+        this.entity = entity;
+        if (this.entity != null) {
+            entity.onContext.listen(_init);
+            _init(entity);
+        }
+        return entity;
     }
 }
