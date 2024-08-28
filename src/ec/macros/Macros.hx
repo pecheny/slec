9b844043e66468d7c5e27e95b218d1c5210a2f67
artem pecheny <pecheny@gmail.com>
2024-08-28 21:01:14 +0300

    Customizable method to call after create from getOrCreate().

--- src/ec/macros/Macros.hx
+++ src/ec/macros/Macros.hx
@@ -5,17 +5,19 @@ import haxe.macro.Context;
 
 #if macro
 class Macros {
-    public static function buildGetOrCreate() {
+    public static function buildGetOrCreate(?onCreate:String) {
         var ct = Context.getLocalClass().get();
         var ttp = @:privateAccess haxe.macro.TypeTools.toTypePath;
         var tp:TypePath = ttp(ct, []);
         var typeExpr:Expr = macro $i{ct.name}; // {expr:EConst(CIdent(ct.name)), pos:Context.currentPos()};
+
+        var bindCall = onCreate?? "bind";
         var body = macro {
             var tg = e.getComponent($typeExpr);
             if (tg != null)
                 return tg;
             tg = new $tp();
-            tg.bind(e);
+            tg.$bindCall(e);
             return tg;
         };
 
