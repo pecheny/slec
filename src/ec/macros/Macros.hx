e492f0b8559f196a32a87be466c53619a25e4977
artem pecheny <pecheny@gmail.com>
2024-09-26 15:00:54 +0300

    GetOrCreate macro suppport of custom constructor args.

--- src/ec/macros/Macros.hx
+++ src/ec/macros/Macros.hx
@@ -1,12 +1,49 @@
 package ec.macros;
 
+import haxe.macro.Type.TFunc;
+import haxe.macro.Type.ClassType;
 import haxe.macro.Expr;
 import haxe.macro.Context;
+
+using haxe.macro.TypeTools;
 
 #if macro
 class Macros {
     public static function buildGetOrCreate(?onCreate:String) {
+        function findCtr(ct:ClassType) {
+            if (ct.constructor != null)
+                return ct.constructor.get();
+            if (ct.superClass != null) {
+                return findCtr(ct.superClass.t.get());
+            }
+            return null;
+        }
+
+        function extractCtrArgs() {
+            var fields = Context.getBuildFields();
+            for (f in fields) {
+                if (f.name == "new")
+                    switch f.kind {
+                        case FFun(f):
+                            return f.args.map(a -> {name: a.name, type: a.type});
+                        case _:
+                            throw "wrong";
+                    }
+            }
+
+            var ctrArgs = switch findCtr(Context.getLocalClass().get())?.type.follow() {
+                case TFun(args, ret): args.map(a -> {name: a.name, type: a.t.toComplexType()});
+                case _: null;
+            }
+            if (ctrArgs != null)
+                return ctrArgs;
+            throw "Magic, there is no constrictor ";
+        }
+
         var ct = Context.getLocalClass().get();
+        var fields = Context.getBuildFields();
+        var ctrArgs = extractCtrArgs();
+
         var ttp = @:privateAccess haxe.macro.TypeTools.toTypePath;
         var tp:TypePath = ttp(ct, []);
         var typeExpr:Expr = macro $i{ct.name}; // {expr:EConst(CIdent(ct.name)), pos:Context.currentPos()};
@@ -16,13 +53,11 @@ class Macros {
             var tg = e.getComponent($typeExpr);
             if (tg != null)
                 return tg;
-            tg = new $tp();
+            tg = new $tp($a{ctrArgs.map(a -> macro $i{a.name})});
             $bindCall;
             return tg;
         };
 
-        var f:Field;
-        var fields = Context.getBuildFields();
         fields.push({
             name: "getOrCreate",
             access: [APublic, AStatic],
@@ -32,7 +67,7 @@ class Macros {
                         name: "e",
                         type: macro :ec.Entity
                     }
-                ],
+                ].concat(ctrArgs),
                 expr: body
             }),
             pos: Context.currentPos()
